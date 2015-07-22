876bec5b2f12aa7a7959c93f489af29883799565
Robert Adams <robert@robertadams.net>
2015-07-21 23:07:05 -0700

    Virtual machine base class.

---
+++ machine.hpp
@@ -0,0 +1,29 @@
+#ifndef machine_hpp_2015_0714_1649
+#define machine_hpp_2015_0714_1649 
+
+#include <vector>
+#include <iostream>
+
+typedef unsigned int word_t;
+typedef std::vector<word_t> segment_t;
+
+typedef segment_t stack_t;
+typedef segment_t code_t;
+
+typedef word_t result_t;
+
+class Machine {
+public:
+  void execute(const code_t& c) { v_execute(c); }
+  void reset() { v_reset(); }
+private:
+  virtual void v_execute(const code_t&) = 0;
+  virtual void v_reset() = 0;
+};
+
+
+#include "smachine.hpp"
+#include "rmachine.hpp"
+
+
+#endif//machine_hpp_2015_0714_1649
