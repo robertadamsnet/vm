0823ecaa4fcd497de17fa78d04906dcee9dfc002
Robert Adams <robert@robertadams.net>
2015-07-21 23:08:30 -0700

    Generates bytecode from an AST.

---
+++ assembler.hpp
@@ -0,0 +1,33 @@
+#ifndef ast_compile_hpp_2015_07_15_2353
+#define ast_compile_hpp_2015_07_15_2353 
+
+#include "ast.hpp"
+
+#include "errors.hpp"
+
+#include "machine.hpp"
+
+class Assembler final  : public Host {
+public:
+  void accept(const EndOfFile&) override
+  {
+
+  }
+
+  void accept(const Instruction& node) override
+  {
+    for(const auto& d : node.data()) {
+      code_.push_back(d);
+    }
+  }
+
+  const code_t& code() const { return code_; }
+
+  void reset() {
+    code_.clear();
+  }
+private:
+  code_t code_;
+};
+
+#endif//ast_compile_hpp_2015_07_15_2353
